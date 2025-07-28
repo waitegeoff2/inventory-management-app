7b5f3cd0254dc29fd2a5d4d63a7c48fe0062ee84
Geoffrey Waite <186988887+waitegeoff2@users.noreply.github.com>
2025-07-28 16:42:26 -0400

    Updating SQL to populate database

--- db/populatedb.js
+++ db/populatedb.js
@@ -5,8 +5,18 @@ const SQL = `
 CREATE TABLE IF NOT EXISTS video_games (
     id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name VARCHAR ( 255 ),
-    description VARCHAR ( 255 ),
     year INT,
+    cover VARCHAR ( 2048 )
+);
+
+INSERT INTO video_games (name, description, year) (
+    ('Dark Souls', '2011', 'https://cdn.thegamesdb.net/images/original/boxart/front/7752-1.jpg'),
+    ('The Legend of Zelda: The Wind Waker', '2003', 'https://cdn.thegamesdb.net/images/original/boxart/front/129800-1.jpg'),
+    ('Elden Ring', '2022', 'https://cdn.thegamesdb.net/images/original/boxart/front/72163-1.jpg'), 
+    ('007: Nightfire', '2002', 'https://cdn.thegamesdb.net/images/original/boxart/front/5428-1.jpg'),
+    ('Shenmue', '2000', 'https://cdn.thegamesdb.net/images/original/boxart/front/62809-1.jpg'),
+    
+
 );
 
 CREATE TABLE IF NOT EXISTS genres (
@@ -19,7 +29,13 @@ CREATE TABLE IF NOT EXISTS developers (
     developer VARCHAR ( 255 ),
 );
 
+CREATE TABLE IF NOT EXISTS games_genres (
 
+);
+
+CREATE TABLE IF NOT EXISTS games_devs (
+
+);
 
 `;
 
--- db/queries.js
+++ db/queries.js
@@ -1,7 +1,9 @@
 const pool = require("./pool")
 
 async function retrieveGames() {
-    const { rows } = await pool.query("SELECT * FROM games")
+    // const { rows } = await pool.query(SELECT STATEMENT WITH INNER JOINS to RETURN game info, genre info, dev info)
+    //need to include a GROUP BY???? so that only the name shows up once with the corresponding other ones
+    
     console.log("games are: ", rows);
     return rows;
 }
