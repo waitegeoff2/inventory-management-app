ff3c832125e8ff88218392f614cdc5f1984d4151
Geoffrey Waite <186988887+waitegeoff2@users.noreply.github.com>
2025-07-28 17:04:58 -0400

    Adding SQL values to populate database

--- db/populatedb.js
+++ db/populatedb.js
@@ -9,16 +9,6 @@ CREATE TABLE IF NOT EXISTS video_games (
     cover VARCHAR ( 2048 )
 );
 
-INSERT INTO video_games (name, description, year) (
-    ('Dark Souls', '2011', 'https://cdn.thegamesdb.net/images/original/boxart/front/7752-1.jpg'),
-    ('The Legend of Zelda: The Wind Waker', '2003', 'https://cdn.thegamesdb.net/images/original/boxart/front/129800-1.jpg'),
-    ('Elden Ring', '2022', 'https://cdn.thegamesdb.net/images/original/boxart/front/72163-1.jpg'), 
-    ('007: Nightfire', '2002', 'https://cdn.thegamesdb.net/images/original/boxart/front/5428-1.jpg'),
-    ('Shenmue', '2000', 'https://cdn.thegamesdb.net/images/original/boxart/front/62809-1.jpg'),
-    
-
-);
-
 CREATE TABLE IF NOT EXISTS genres (
     id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     genre VARCHAR ( 255 ),   
@@ -30,12 +20,44 @@ CREATE TABLE IF NOT EXISTS developers (
 );
 
 CREATE TABLE IF NOT EXISTS games_genres (
-
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    game_id INTEGER REFERENCES video_games(id),
+    genre_id INTEGER REFERENCES genres(id)
 );
 
 CREATE TABLE IF NOT EXISTS games_devs (
-
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    game_id INTEGER REFERENCES video_games(id),
+    dev_id INTEGER REFERENCES developers(id)
 );
+
+INSERT INTO video_games (name, year, cover)
+VALUES
+    ('Dark Souls', '2011', 'https://cdn.thegamesdb.net/images/original/boxart/front/7752-1.jpg'),
+    ('The Legend of Zelda: The Wind Waker', '2003', 'https://cdn.thegamesdb.net/images/original/boxart/front/129800-1.jpg'),
+    ('Elden Ring', '2022', 'https://cdn.thegamesdb.net/images/original/boxart/front/72163-1.jpg'), 
+    ('007: Nightfire', '2002', 'https://cdn.thegamesdb.net/images/original/boxart/front/5428-1.jpg'),
+    ('Shenmue', '2000', 'https://cdn.thegamesdb.net/images/original/boxart/front/62809-1.jpg'),
+    ('Kingdom Come: Deliverance 2', '2025', 'https://cdn.thegamesdb.net/images/original/boxart/front/126214-1.jpg'),
+    ('NBA 2K9', '2008', 'https://cdn.thegamesdb.net/images/original/boxart/front/14334-1.jpg'),
+    ('Resident Evil 4', '2005', 'https://cdn.thegamesdb.net/images/original/boxart/front/89870-1.jpg'),
+    ('Spelunky 2', '2020', 'https://cdn.thegamesdb.net/images/original/boxart/front/96166-1.jpg'),
+    ('Hotline Miami', '2012', 'https://cdn.thegamesdb.net/images/original/boxart/front/13958-1.jpg');
+
+INSERT INTO genres (genre)
+VALUES
+('FromSoftware'),
+('Nintendo'),
+('Electronic Arts'),
+('Eurocom'),
+('Sega'),
+('Warhorse'),
+('2K'),
+('Visual Concepts'),
+('Capcom'),
+('Moss Mouth'),
+('Blitworks'),
+('Denton');
 
 `;
 
